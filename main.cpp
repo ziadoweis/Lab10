c7d6d6e4e5b8aecece9407df84b031fac66addd8
Justin Lin <jchlin06@gmail.com>
2023-12-01 16:42:36 -0500

    Lab10 Template

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <ctime>
+#include <chrono>
+#include <thread>
+#include <string>
+#include <iostream>
+#include <unordered_set>
+#include "HashTable.h"
+
+using namespace std;
+typedef chrono::high_resolution_clock Clock;
+
+int main() {
+    return 0;
+}
